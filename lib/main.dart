23575d613bc3849074cbdd23d08469748e28708e
Kilian Schulz <schulz.kilian@outlook.de>
2023-03-07 11:31:29 +0100

    implementing a backend database for the purchases

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'persistentstorage.dart';
 
 void main() => runApp(new Myapp());
 
@@ -39,37 +40,51 @@ class _HomeState extends State<_Home> {
             actions: [
               GestureDetector(
                   onTap: () => showDialog<String>(
-        context: context,                
-                    builder:(BuildContext context) => AlertDialog(
-                          title: const Text('New Entry'),
-                          actions: <Widget>[
-                            Center(child:Container(width: 700,height: 500,
-                            color: Color.fromARGB(0, 255, 255, 255),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 100),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                      labelText: 'Transaction Value',
-  ),),),
-                              Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 100),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                      labelText: 'Type of transaction',
-  )
-                              ),),
-                              GestureDetector(child: Container(
-                                decoration: BoxDecoration(color: Color.fromARGB(255, 43, 161, 0),borderRadius: BorderRadius.all(Radius.circular(20))),
-                                width: 80,
-                                height: 60,
-                                child: Center(child:Text("Enter")),
-                              ),)
-                              ]
-                            ),
-                            ))
-                            
-                          ])),
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                              title: const Text('New Entry'),
+                              actions: <Widget>[
+                                Center(
+                                    child: Container(
+                                  width: 700,
+                                  height: 500,
+                                  color: Color.fromARGB(0, 255, 255, 255),
+                                  child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Padding(
+                                          padding:
+                                              EdgeInsets.fromLTRB(0, 0, 0, 100),
+                                          child: TextField(
+                                            decoration: InputDecoration(
+                                              labelText: 'Transaction Value',
+                                            ),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                              EdgeInsets.fromLTRB(0, 0, 0, 100),
+                                          child: TextField(
+                                              decoration: InputDecoration(
+                                            labelText: 'Type of transaction',
+                                          )),
+                                        ),
+                                        GestureDetector(
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: Color.fromARGB(
+                                                    255, 43, 161, 0),
+                                                borderRadius: BorderRadius.all(
+                                                    Radius.circular(20))),
+                                            width: 80,
+                                            height: 60,
+                                            child: Center(child: Text("Enter")),
+                                          ),
+                                        )
+                                      ]),
+                                ))
+                              ])),
                   child: Container(
                     height: 0,
                     width: 50,
